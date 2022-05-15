b229dc9ce10845398063941fd0e09d6ea1d0c844
Fernando Macas <74120117+MLxFernando@users.noreply.github.com>
2022-05-15 15:00:23 -0500

    Delete index.html

---
+++ online-test/src/test/java/com/online/test/models/Exam.java
@@ -0,0 +1,17 @@
+package com.online.test.models;
+
+import javax.persistence.*;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "TBL_EXAMS")
+@Getter
+@Setter
+
+public class Exam{
+    
+
+}
+
