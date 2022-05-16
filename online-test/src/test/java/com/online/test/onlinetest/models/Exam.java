544c06cf6868fbd1eed08eb01f3b90539d5d5a14
MLxFernando <fernandomacas68@gmail.com>
2022-05-16 07:39:53 -0500

    update

--- online-test/src/test/java/com/online/test/onlinetest/models/Exam.java
+++ online-test/src/test/java/com/online/test/onlinetest/models/Exam.java
@@ -1,5 +1,45 @@
 package com.online.test.onlinetest.models;
 
+import java.util.List;
+
+import javax.persistence.Column;
+
+//import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "TBL_EXAMS")
+@Getter
+@Setter
 public class Exam {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "TITLE", nullable = false, length = 100)
+    private String title;
+    @Column(name = "DESCRIPTION")
+    private String description;
+    @Column(name = "TIME_LIMIT")
+    private short timeLimit;
+    @Column(name = "MINIMUM_PASSING_SCORE", precision = 4, scale = 2)
+    private double minimumPassingScore;
+    @Column(name = "NUMBER_OF_QUESTIONS")
+    private short numberOfQuestions;
+    @Column(name = "INSTRUCTIONS")
+    private String instructions;
+    //private LocalDate date;
+
     
+    //anotacion
+    @OneToMany(mappedBy = "exam")       //el nombre del atributo en la clase B question, A es la clase examen.
+    private List<Question> questions;
 }
--- online-test/src/test/java/com/online/test/onlinetest/models/Question.java
+++ online-test/src/test/java/com/online/test/onlinetest/models/Question.java
@@ -1,5 +1,36 @@
 package com.online.test.onlinetest.models;
 
+import javax.persistence.Column;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "TBL_QUESTION")
+@Getter
+@Setter
 public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "NAME")
+    private String name;
+    @Column(name = "DESCRIPTION")
+    private String description;
+    @Column(name = "SCORE")
+    private short score;
+
+    @ManyToOne
+    @JoinColumn(name = "EXAM_ID", nullable = false)  //clave primaria es "EXAM_ID", FK O foreign key.
+    private Exam exam;
     
 }
