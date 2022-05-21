6e24b0e77b5e5a1ce9c4d2a4bb62b69856cc4fff
MLxFernando <fernandomacas68@gmail.com>
2022-05-21 17:43:25 -0500

    Update

--- online-test/src/test/java/com/online/test/onlinetest/ApplicationConfig.java
+++ online-test/src/test/java/com/online/test/onlinetest/ApplicationConfig.java
@@ -1,5 +1,7 @@
 package com.online.test.onlinetest;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 //para mappear
--- online-test/src/test/java/com/online/test/onlinetest/models/Option.java
+++ online-test/src/test/java/com/online/test/onlinetest/models/Option.java
@@ -1,6 +1,6 @@
 package com.online.test.onlinetest.models;
 
-import java.util.List;
+//import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -9,7 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+//import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Getter;
