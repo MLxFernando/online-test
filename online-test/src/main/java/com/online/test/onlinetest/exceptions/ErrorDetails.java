202275d896e8c2c3b1f0bf9e6483143edb6376df
MLxFernando <fernandomacas68@gmail.com>
2022-06-01 17:11:57 -0500

    Update

--- online-test/src/main/java/com/online/test/onlinetest/exceptions/ErrorDetails.java
+++ online-test/src/main/java/com/online/test/onlinetest/exceptions/ErrorDetails.java
@@ -1,13 +1,13 @@
 package com.online.test.onlinetest.exceptions;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
+@AllArgsConstructor
 public class ErrorDetails {
     private String message;
-    private String details;
+    private String details;    
 }
